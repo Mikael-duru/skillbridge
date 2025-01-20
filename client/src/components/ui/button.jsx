13c6cb6463ceabb3b715427b1dfc93c656afbfcf
Duru C. Michael <durumikaelc@gmail.com>
2025-01-20 10:55:48 +0100

    increase padding-y for tablets above

--- client/src/components/ui/button.jsx
+++ client/src/components/ui/button.jsx
@@ -15,12 +15,12 @@ const buttonVariants = cva(
 					"bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
 				outline:
 					"border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
-				secondary: "bg-[#008080] text-white shadow-sm hover:bg-[#006060]",
-				ghost: "hover:bg-gray-100 hover:text-accent-foreground",
+				secondary: "bg-[#009090] text-white shadow-sm hover:bg-[#107e7d]",
+				ghost: "hover:bg-[#107e7d]/5 hover:text-accent-foreground",
 				link: "text-primary underline-offset-4 hover:underline",
 			},
 			size: {
-				default: "h-9 px-4 py-2",
+				default: "h-9 px-4 py-2 sm:py-5",
 				sm: "h-8 rounded-md px-3 text-xs",
 				lg: "h-10 rounded-md px-8",
 				icon: "h-9 w-9",
