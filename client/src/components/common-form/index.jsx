36d2243261c205cbfcdcd3a4910d69720882fc21
Duru C. Michael <durumikaelc@gmail.com>
2025-06-29 19:41:50 +0100

    add forgot password button

--- client/src/components/common-form/index.jsx
+++ client/src/components/common-form/index.jsx
@@ -8,6 +8,7 @@ function CommonForm({
 	formData,
 	setFormData,
 	isButtonDisabled = false,
+	setChangePasswordTab = () => {},
 }) {
 	return (
 		<form onSubmit={handleSubmit}>
@@ -17,9 +18,19 @@ function CommonForm({
 				formData={formData}
 				setFormData={setFormData}
 			/>
+			{buttonText === "Sign In" && (
+				<p className="mt-2 text-sm text-gray-500 text-end">
+					<a
+						className="cursor-pointer hover:underline"
+						onClick={setChangePasswordTab}
+					>
+						Forgot password?
+					</a>
+				</p>
+			)}
 			<Button
 				type="submit"
-				className="w-full mt-5 tracking-wide font-bold"
+				className="w-full mt-5 font-bold tracking-wide"
 				variant={`${!isButtonDisabled ? "secondary" : "default"}`}
 				disabled={isButtonDisabled}
 			>
